4eda957e4296adc010358ec902e830b99272b6a4
ssnukala <snthelion@gmail.com>
2022-11-16 22:57:19 -0600

    Update uf-jqueryvalidation-config.js

--- assets/userfrosting/js/uf-jqueryvalidation-config.js
+++ assets/userfrosting/js/uf-jqueryvalidation-config.js
@@ -33,6 +33,10 @@ jQuery.validator.setDefaults({
     errorPlacement: function(error, element) {
         if (element.hasClass('js-select2') && element.next('.select2-container').length) {
             error.insertAfter(element.next('.select2-container'));
+        } else if (element.hasClass('crud-select2') && element.next('.select2-container').length) {
+            error.insertAfter(element.next('.select2-container'));
+        } else if (element.hasClass('select2') && element.next('.select2-container').length) {
+            error.insertAfter(element.next('.select2-container'));
         } else if (element.parent('.input-group').length) {
             error.insertAfter(element.parent());
         } else if (element.prop('type') === 'radio' && element.parent('.radio-inline').length) {
